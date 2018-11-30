a87d78ae83e4f06d54fda56762c1ef57905e037d
janooosh <jan.hapunkt@gmail.com>
2018-11-30 07:13:37 +0100

    corrected links impressum/datenschutz on register template

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -191,9 +191,8 @@
         <footer class="my-5 pt-5 text-muted text-center text-small">
           <p class="mb-1">&copy; 2018 Studenten im Olympiazentrum e.V.</p>
           <ul class="list-inline">
-            <li class="list-inline-item"><a href="#">Privacy</a></li>
-            <li class="list-inline-item"><a href="#">Terms</a></li>
-            <li class="list-inline-item"><a href="#">Support</a></li>
+            <li class="list-inline-item"><a href="{{route('impressum')}}">Impressum</a></li>
+            <li class="list-inline-item"><a href="{{route('datenschutz')}}">Datenschutz</a></li>
           </ul>
         </footer>
       </div>
