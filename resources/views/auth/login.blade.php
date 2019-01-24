25e55a224d570e8833c3e3b255f49624e99e7408
janooosh <jan.hapunkt@gmail.com>
2019-01-24 12:04:22 +0100

    wartung

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.intro')
 
 @section('content')
+<div class="row">
+    <div class="alert alert-info">
+       <b>Wartungsarbeiten</b><br />
+       Wie angekündigt, finden heute, Donnerstag, 24.01., Wartungsarbeiten statt. Das Tool steht ab Freitag, 25.1., 08:00h, wieder zur Verfügung. <b r/>
 
+    </div>
+    
+</div>
+<div class="row">
+    
+</div>
+
+{{--
 <form class="form-signin" method="POST" action="{{ route('login') }}">
 @csrf    
     <img class="mb-4" src="{{ asset('img/ananas.png') }}" alt="" width="100" height="auto">
@@ -36,4 +48,5 @@
         <a class="btn btn-primary btn-lg btn-block" href="{{ route('register') }}">{{ __('Neu hier?') }}</a></p>
         <p class="mt-3 mb-5 text-muted">&copy; 2018 | Studenten im Olympiazentrum e.V. <br /> <a href="{{route('impressum')}}" target="_blank">Impressum</a> | <a href="{{route('datenschutz')}}" target="_blank">Datenschutz</a></p>
 </form>
+--}}
 @endsection
